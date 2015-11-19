acdcc96ae7d0453f8029636e362f1c4692af0299
delgaka <delgaka@users.noreply.github.com>
2015-11-19 21:33:39 -0200

    Refactoring

--- packagemon.py
+++ packagemon.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
+
+import re
+import subprocess
+import os
+import socket
+
 # -*- coding: utf-8 -*-
 
 # Project Name: PackageMon
 # File Name:    packagemon
 
 __author__ = 'delgaka'
-
-import re
-import subprocess
-import os
-import socket
 
 
 def atualiza():
@@ -20,29 +21,29 @@ def atualiza():
     subprocess.call(["apt-get", "update", "-qq"])
 
 
-def definir_data():
+def setting_system_date():
     """
     Get the system date (date -u)
     """
 
-    data = subprocess.Popen(["date", "-u"], stdout=subprocess.PIPE)
-    data, dataerr = data.communicate()
-    data = data.replace("\n", "")
-    return data
+    date_system = subprocess.Popen(["date", "-u"], stdout=subprocess.PIPE)
+    date_system, date_err = date_system.communicate()
+    date_system = date_system.replace("\n", "")
+    return date_system
 
 
-def definir_hostname():
+def setting_local_hostname():
     """
     Getting hostname.
     """
 
-    hostname = subprocess.Popen(["hostname"], stdout=subprocess.PIPE)
-    hostname, hostnameerr = hostname.communicate()
-    hostname = hostname.replace("\n", "")
-    return hostname
+    local_hostname = subprocess.Popen(["hostname"], stdout=subprocess.PIPE)
+    local_hostname, local_hostname_err = local_hostname.communicate()
+    local_hostname = local_hostname.replace("\n", "")
+    return local_hostname
 
 
-def definir_ip():
+def setting_host_ip():
     """
     Getting ip address (hostname -I)
     """
@@ -55,26 +56,27 @@ def definir_ip():
     return ip
 
 
-def definir_info_system():
+def setting_system_info():
     """
     Running the lsb_release -a for system OS distribution
     """
 
+    global output_shell
     try:
         shell1 = subprocess.Popen(["lsb_release", "-a"],
                                   stdout=subprocess.PIPE)
         shell2 = subprocess.Popen(["cut", "-f", "2"],
                                   stdin=shell1.stdout, stdout=subprocess.PIPE)
-        output, err = shell2.communicate()
+        output_shell, err = shell2.communicate()
         shell2.stdout.close()
         # output,err = shell1.communicate()
         shell1.stdout.close()
-        output = output.split("\n")
+        output_shell = output_shell.split("\n")
     except Exception, e:
         print("Somethin goes wrong on the definir_pacotes_security()", e)
 
-    if '' in output:
-        output.remove('')
+    if '' in output_shell:
+        output_shell.remove('')
 
     """
     Output:
@@ -84,121 +86,124 @@ def definir_info_system():
     3 - Codename
     """
 
-    dist_id = output[0]
+    dist_id = output_shell[0]
     print(dist_id)
-    description = output[1]
+    description = output_shell[1]
     print(description)
-    release = output[2]
+    release = output_shell[2]
     print(release)
-    codename = output[3]
+    codename = output_shell[3]
     print(codename)
 
-    print(output)
+    print(output_shell)
 
 
-def definir_pacotes_security():
+def setting_security_packages():
     """
     Running the apt-show-versions to list upgreadables pacakges.
-    Formating the result in comma separated.
-    (package,architeture,repo,cuurent_version,new_version)
+    Formatting the result in comma separated.
+    (package,architecture,repo,current_version,new_version)
     """
 
+    global output_shell
     try:
         shell1 = subprocess.Popen(["apt-show-versions", "-u"],
                                   stdout=subprocess.PIPE)
         # shell2 = subprocess.Popen(["grep", "\-security"],
         #    stdin = shell1.stdout, stdout = subprocess.PIPE)
-        # output,err = shell2.communicate()
+        # output_shell,err = shell2.communicate()
         # shell2.stdout.close()
-        output, err = shell1.communicate()
+        output_shell, err = shell1.communicate()
         shell1.stdout.close()
-        output = output.split("\n")
+        output_shell = output_shell.split("\n")
     except Exception, e:
         print("Package apt-show-versions not installed")
         subprocess.call(["apt-get", "install", "-qq", "apt-show-versions"])
-        definir_pacotes_security()
+        setting_security_packages()
 
-    texto = []
+    text = []
 
     # removing the blank item (garbage).
-    if '' in output:
-        output.remove('')
+    if '' in output_shell:
+        output_shell.remove('')
 
-    # formating the result
-    for i in output:
-        pacote = re.search("^([^:]+)", i).group(0)
-        arquitetura = re.search("(?<=:)\w+", i).group(0)
-        repositorio_temp = re.search("(?<=\/)\S+", i).group(0)
+    # formatting the result
+    for x in output_shell:
+        package = re.search("^([^:]+)", x).group(0)
+        platform = re.search("(?<=:)\w+", x).group(0)
+        repository_temp = re.search("(?<=\/)\S+", x).group(0)
 
         try:
-            # print(repositorio_temp)
-            temp = repositorio_temp.split("-")
-            repositorio = repositorio_temp
+            # print(repository_temp)
+            temp = repository_temp.split("-")
+            repository = repository_temp
             if temp[0] == "Security":
                 repo_type = str(1)
             else:
                 repo_type = str(0)
         except Exception, e:
-            repositorio = repositorio_temp
+            repository = repository_temp
             repo_type = 0
 
-        atual = re.search("(?<=\s)\S+", i).group(0)
+        atual = re.search("(?<=\s)\S+", x).group(0)
         if atual == "*manually*":
-            versao_atual = re.search("(?<=from )\S+", i).group(0)
+            versao_atual = re.search("(?<=from )\S+", x).group(0)
             manual = str(1)
         else:
             versao_atual = atual
             manual = str(0)
 
-        versao_nova = re.search("(?<= to ).*", i).group(0)
+        versao_nova = re.search("(?<= to ).*", x).group(0)
 
         '''
         Fields:
-        pacote, arquitetura, repositorio, repo_type, versao_atual,versao_nova
+        package, platform, repository, repo_type, versao_atual,versao_nova
         '''
-        a = (pacote + "," + arquitetura + "," + repositorio + ","
-             + repo_type + "," + manual + "," + versao_atual + "," + versao_nova)
+        a = (package + "," + platform + "," + repository + "," +
+             repo_type + "," + manual + "," + versao_atual + "," + versao_nova)
 
-        texto.append(a)
+        text.append(a)
 
-    return texto
+    return text
 
 
-def escreve_log(texto):
+def write_log(text):
     """
     Writing the log. /var/log/packagemon
+    :param text:
     """
 
     try:
-        log = open('/var/log/packagemon/packagemon.log', 'w')
-        log.write(texto)
-        log.close()
+        log_text = open("/var/log/packagemon/packagemon.log", 'w')
+        log_text.write(text)
+        log_text.close()
     except:
         path = "/var/log/packagemon"
         os.makedirs(path, 0755)
-        log = open('/var/log/packagemon/packagemon.log', 'w')
-        log.close()
-        escreve_log(texto)
-        return texto
+        log_text = open('/var/log/packagemon/packagemon.log', 'w')
+        log_text.close()
+        write_log(text)
+        return text
 
 
-def envia_log(log):
+def envia_log(self):
     """
     Sending log to SIEM/BigData
     sock.sendto(<object>, ("<hostname>", <destination_port>))
+    :param self:
     """
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # sock.bind(socket.gethostname(), 5055)
-    sock.sendto(log, ("delgaka", 5000))
+    sock.sendto(self, ("delgaka", 5000))
 
 
 # atualiza()
-data = definir_data()
-hostname = definir_hostname()
-output = definir_pacotes_security()
+data = setting_system_date()
+hostname = setting_local_hostname()
+output = setting_security_packages()
 # ip = definir_ip()
-definir_info_system()
+setting_system_info()
 log = ''
 
 for i in output:
@@ -212,5 +217,5 @@ repo_type (1 - Security, 0 - Non Security),
 manual (1 - manually update, 0 - Non manually), versao_atual,versao_nova
 '''
 print(log)
-escreve_log(log)
+write_log(log)
 envia_log(log)
